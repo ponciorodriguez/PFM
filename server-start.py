b270f42a825d3f26757df1d1b067281613a4ecbf
ponciorodriguez <nelipro2020@gmail.com>
2021-09-09 14:44:43 +0200

    windows +  linux server call

--- server-start.py
+++ server-start.py
@@ -1,4 +1,4 @@
 import subprocess
 
-subprocess.call('sudo python3 -m http.server 8080', shell=True)
-
+subprocess.call('python -m http.server 8080', shell=True)
+# subprocess.call('sudo python3 -m http.server 8080', shell=True)
